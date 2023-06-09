d33b60369eaa64bae64ba46a976c0bc755cf22a6
Pablo Treo <pablotreo@gmail.com>
2023-06-08 23:24:37 -0300

    github

--- next.config.js
+++ next.config.js
@@ -1,20 +1,3 @@
-// module.exports = {
-//   reactStrictMode: true,
-// }
-
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: 'export',
-  images: {
-    loader: 'cloudinary',
-    path: '/',
-  },
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-};
- 
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+}
