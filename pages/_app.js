820d9199edecda96ace10830110891ceeb18460c
Pablo Treo <pablotreo@gmail.com>
2023-06-09 00:01:36 -0300

    dale

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,11 @@ function MyApp({ Component, pageProps }) {
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
           crossOrigin="anonymous"
         />
-        
+        <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossOrigin="anonymous"
+      />
         <meta
           name="keywords"
           content="cabañas, alojamiento, camping, motorhome, bella vista, iglesia, san juan"
